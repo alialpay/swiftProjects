ad0c8099416315287826e872df5a0ade10a36118
alialpay <alialpay@turksat.com.tr>
2024-08-01 09:25:27 +0300

    Initial Commit

---
+++ firstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firstApp
+//
+//  Created by Ali Alpay on 1.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
