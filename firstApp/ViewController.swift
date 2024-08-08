fd0541e8a4b04094ad3f8d87670f4826ce518d8c
alialpay <alialpay96@hotmail.com>
2024-08-08 11:24:48 +0300

    update

--- firstApp/ViewController.swift
+++ firstApp/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+@IBAction func buttonClicked(_ sender: Any){
+    myLabel.text = "Ali Alpay"
+    }
 }
 
